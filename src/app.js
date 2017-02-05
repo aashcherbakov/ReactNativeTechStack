7a890e5373940d82cee91372bfd25a2d82f84dda
Alex <oo.shcherbakov@gmail.com>
2017-02-04 21:53:03 -0500

    Add list view

--- src/app.js
+++ src/app.js
@@ -3,11 +3,18 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux'; 
 import reducers from './reducers';
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 const App = () => {
-  <Provider store={createStore(reducers)}>
-    <View />
-  </Provider>
+  return (
+    <Provider store={createStore(reducers)}>
+      <View style={{ flex: 1 }}>
+        <Header headerText='Tech Stack' />
+        <LibraryList />
+      </View>
+    </Provider>
+  );
 };
 
 export default App;
